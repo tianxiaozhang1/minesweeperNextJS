8e48a6bd00c9aa2ee5acf630b40666c0e7f9eec5
Tianxiao Zhang <tianxiaozhang@gmail.com>
2025-09-15 13:48:25 -0400

    Updating

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-=======
 const config = {
   plugins: ["@tailwindcss/postcss"],
->>>>>>> b8816dfe3849b08855ebfb37edeb5c2f613de7e7
 };
 
 export default config;
